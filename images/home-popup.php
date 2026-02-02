06d9372e7f0716b825075de33e8ca6ce2aee34d7
anuragtawaniya <53399643+Anuragtawaniya@users.noreply.github.com>
2026-02-02 16:32:13 +0530

    Add files via upload

---
+++ images/home-popup.php
@@ -0,0 +1,18 @@
+<div class="bodyoverlaybgcons"></div> 
+<div class="modal fade enquiremodel" id="enquirepopmain" tabindex="-1" aria-labelledby="enquirepoplLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-bs-dismiss="modal" aria-label="Close">
+                    <img class="fancybox-closess" src="images/closewh.svg" alt="Close Icon" width="25" height="25">
+                </button>
+            </div>
+            <div class="modal-body p-0">
+				<a href="javascript:void(0);" target="_blank">
+					<img src="images/Techspark-event.webp" alt="">
+				</a>
+            </div>
+        </div>
+    </div>
+</div>
+    
